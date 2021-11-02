28a62a7497cf1057e75cc3c52b6559c28aae8999
Desktop\Artem <artem.slobodianiuk@gmail.com>
2021-11-02 13:25:37 +0200

    Second registration form filling in

--- TestNewbookmodels/TestNewbookmodels/UnitTest1.cs
+++ TestNewbookmodels/TestNewbookmodels/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -20,24 +21,40 @@ namespace TestNewbookmodels
         [Test]
         public void SignupTest()
         {
+            DateTime dateTimeNow = DateTime.Now;
+            string stringDateTimeNow = dateTimeNow.ToShortDateString() + "." 
+                + dateTimeNow.Hour.ToString() + dateTimeNow.Minute.ToString() + dateTimeNow.Second.ToString();
             IWebElement navBarSignUpButton = driver.FindElement(By.ClassName("Navbar__signUp--12ZDV"));
             navBarSignUpButton.Click();
-            IWebElement firstNameField = driver.FindElement(By.Name("first_name"));
+            IWebElement firstNameField = driver.FindElement(By.CssSelector("[name='first_name']"));
             firstNameField.SendKeys("Abdula");
-            IWebElement lastNameField = driver.FindElement(By.Name("last_name"));
+            IWebElement lastNameField = driver.FindElement(By.CssSelector("[name='last_name']"));
             lastNameField.SendKeys("Shchur");
-            IWebElement emailField = driver.FindElement(By.Name("email"));
-            emailField.SendKeys("abdula.shchur@gmail.com");
-            IWebElement passwordField = driver.FindElement(By.Name("password"));
+            IWebElement emailField = driver.FindElement(By.CssSelector("[name='email']"));
+            emailField.SendKeys($"abdula.shchur.{stringDateTimeNow}@gmail.com");
+            IWebElement passwordField = driver.FindElement(By.CssSelector("[name='password']"));
             passwordField.SendKeys("Qwerty1!");
-            IWebElement passwordConfirmField = driver.FindElement(By.Name("password_confirm"));
+            IWebElement passwordConfirmField = driver.FindElement(By.CssSelector("[name='password_confirm']"));
             passwordConfirmField.SendKeys("Qwerty1!");
-            IWebElement phoneNumberField = driver.FindElement(By.Name("phone_number"));
+            IWebElement phoneNumberField = driver.FindElement(By.CssSelector("[name='phone_number']"));
             phoneNumberField.SendKeys("3425634634");
-            //IWebElement refferalCodeField = driver.FindElement(By.Name("code"));
-            //refferalCodeField.SendKeys("A34Fg");
             IWebElement signupNextButton = driver.FindElement(By.CssSelector("[type='submit']"));
             signupNextButton.Click();
+            IWebElement companyNameField = driver.FindElement(By.CssSelector("[name='company_name']"));
+            companyNameField.SendKeys("Abdula Inc.");
+            IWebElement companyWebsiteField = driver.FindElement(By.CssSelector("[name='company_website']"));
+            companyWebsiteField.SendKeys("abdulashchur.com");
+            IWebElement locationField = driver.FindElement(By.CssSelector("[name='location']"));
+            locationField.SendKeys("FGH Building, Harrison Avenue, Boston, MA, USA");
+            Thread.Sleep(1000);
+            locationField.SendKeys(Keys.ArrowDown);
+            locationField.SendKeys(Keys.Enter);
+            IWebElement industryInput = driver.FindElement(By.CssSelector("[name='industry']"));
+            industryInput.Click();
+            industryInput.SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+            IWebElement signupFinishButton = driver.FindElement(By.CssSelector("[type='submit']"));
+            signupFinishButton.Click();
             Assert.Pass();
         }
 
